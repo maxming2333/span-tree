0c49772efba8696cb9a6713cb70008512e692ccd
maxming2333 <maxming2333@gmail.com>
2024-06-03 00:18:20 +0800

    feat: get gon object flag

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -11,16 +11,13 @@ const proxyStore = new Store();
 const anchor = document.createElement("div");
 anchor.id = "rcr-anchor";
 
-if (document.querySelectorAll('[aria-label="Loading"]').length === 0) {
-  mount();
-} else {
-  let intervalId = null;
-  intervalId = setInterval(() => {
-    if (document.querySelectorAll('[aria-label="Loading"]').length === 0) {
-      clearInterval(intervalId);
-      mount();
-    }
-  }, 500);
+const isGitlabPage = function() {
+  const headList = Array.from(document.head.querySelectorAll('script'));
+  const gonEl = headList.find(item => item.innerHTML.includes('gon.gitlab_url'));
+  if (gonEl) {
+    return true;
+  }
+  return document.querySelectorAll('[aria-label="Loading"]').length === 0;
 }
 
 const mount = () => {
@@ -43,5 +40,17 @@ const mount = () => {
     });
   }
 };
+
+if (isGitlabPage()) {
+  mount();
+} else {
+  let intervalId = null;
+  intervalId = setInterval(() => {
+    if (isGitlabPage()) {
+      clearInterval(intervalId);
+      mount();
+    }
+  }, 500);
+}
 
 export default proxyStore;
